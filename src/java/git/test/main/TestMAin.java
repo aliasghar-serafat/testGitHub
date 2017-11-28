184b714e89ce66eae0caf22a3dd76fa6077f6bf1
aserafat <aliasghar_serafat@yahoo.com>
2017-11-28 17:18:53 +0330

    test Commit message

--- src/java/git/test/main/TestMAin.java
+++ src/java/git/test/main/TestMAin.java
@@ -1,4 +1,14 @@
 package java.git.test.main;
 
 public class TestMAin {
+
+    @Override
+    public String toString() {
+        return super.toString();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
