c4c4d3728e8999c8c708dd0b2c48089ea7845c45
Ali Asghar Serafat <aliasghar.serafat@gmail.com>
2018-10-23 11:16:08 +0330

    saasasdasasassasassas

--- src/java/git/test/main/TestMAin - Copy (2).java
+++ src/java/git/test/main/TestMAin - Copy (2).java
@@ -11,30 +11,19 @@ public class TestMAin {
     public static void main(String[] args) {
         System.out.println("tessadfft789");
 		System.out.println("451tsdfsdfest");
-		System.out.println("te234234dfsfdst");
 		System.out.println("t4234sdfsdfest");
 		System.out.println("te234234ssdfsdt");
 	System.out.println("te234234dfsfdst");
 
-	System.out.println("te2342345sfdfsdf67567dfsfdst");
-	
-	
+	System.out.println("te234234567567dfsfdst");
 	System.out.println("te234234dfsfdst");
-	System.out.println("te2342345sfdfsdf67567dfsfdst");
-	System.out.println("te234234dfsfdst");
-	System.out.println("te2342345sfdfsdf67567dfsfdst");
 	System.out.println("te234233456464dfsfdst");
 	
 	
 	System.out.println("te234234dfsfdst");
 	System.out.println("te234234dfsfdst");
 	
-	
-	
 		System.out.println("te234234dfsfdst");
-		System.out.println("te2342345sfdfsdf67567dfsfdst");
-	System.out.println("te234234dfsfdst");
-	System.out.println("te2342345sfdfsdf67567dfsfdst");
 	System.out.println("te234234dfsfdst");
 	System.out.println("te234234dfsfdst");
 	
