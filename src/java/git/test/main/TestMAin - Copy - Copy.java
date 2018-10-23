1a9b7ef34a27c7cc39d61f8a970f669b0145a7e0
Ali Asghar Serafat <aliasghar.serafat@gmail.com>
2018-10-23 11:07:54 +0330

    add new class

---
+++ src/java/git/test/main/TestMAin - Copy - Copy.java
@@ -0,0 +1,31 @@
+package java.git.test.main;
+
+public class TestMAin {
+
+    @Override
+    public String toString() {
+        return super.toString();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("tessadfft789");
+		System.out.println("451tsdfsdfest");
+		System.out.println("t4234sdfsdfest");
+		System.out.println("te234234ssdfsdt");
+	System.out.println("te234234dfsfdst");
+
+	System.out.println("te234234567567dfsfdst");
+	System.out.println("te234234dfsfdst");
+	System.out.println("te234233456464dfsfdst");
+	
+	
+	System.out.println("te234234dfsfdst");
+	System.out.println("te234234dfsfdst");
+	
+		System.out.println("te234234dfsfdst");
+	System.out.println("te234234dfsfdst");
+	System.out.println("te234234dfsfdst");
+	
+    }
+}
